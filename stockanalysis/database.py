ac38065df7c5c19d8e6b0059f6b87e60570f25fa
mukesh kumar <msingh.mukesh@gmail.com>
2022-02-19 14:30:19 +0100

    Mukesh db changes

---
+++ stockanalysis/database.py
@@ -0,0 +1,95 @@
+from pickle import NONE
+import mysql.connector as connection
+from mysql.connector.constants import ClientFlag
+import pandas as pd
+from param import config
+
+
+class MySQLDB:
+
+
+    def getConnection(self):
+        conn=connection.connect(**config)
+        self.connection = conn
+        return conn;
+
+    def closeConnection(self):
+        self.connection.close()
+
+    def getData_df(self,query):
+        try:
+            conn = self.getConnection()
+            #query = "Select * from studentdetails;"
+            result_dataFrame = pd.read_sql(query,conn)
+            conn.close() #close the connection
+        except Exception as e:
+            self.connection.close()
+            print(str(e))
+        return result_dataFrame
+    #insert or updte for single record or single dictionary
+    def InsertUpdateData(self,query,data):
+        conn=self.getConnection()
+        _cursor = conn.cursor()
+        # data could be list or dictionary
+        _cursor.execute(query, data)
+        conn.commit()  # and commit changes
+        conn.close()
+    #Create a object like table or view etc
+    def ExecuteDDLStatement(self,createStatement):
+        _conn = self.getConnection()
+        _cursor = _conn.cursor()  # initialize connection cursor
+        #createStatement = "CREATE TABLE stocksdb.tet ( ID INT NOT NULL, Date date NOT NULL)"
+        _cursor.execute(createStatement)  # create a new 'testdb' database
+        _conn.commit()
+        _conn.close()  # close connection
+
+    #Save dataframe to table
+    def SaveDFToTable(self,TableName,df):
+        conn = self.getConnection()
+        _cursor = conn.cursor()
+        print(list(df.to_records(index=False)))
+        _cursor.executemany(query, df)
+        conn.commit()  # and commit changes
+        conn.close()
+
+    def SaveDictionayToTable(self,TableName,dataDict):
+        conn = self.getConnection()
+        _cursor = conn.cursor()
+        df = pd.DataFrame.from_dict(dataDict)
+        print(list(df.to_records(index=False)))
+        #df.to_sql(TableName,conn)
+        _cursor.executemany(query, (df.to_records(index=False)))
+        conn.commit()  # and commit changes
+        conn.close()
+
+if __name__ == "__main__":
+    mysql=MySQLDB()
+    df=mysql.getData_df("SELECT * FROM stocksdb.StocksList;")
+    print(df)
+    query = ("INSERT INTO stocksdb.StocksList ( StockName,StockCode,exchange) "
+             "VALUES (%s, %s, %s)")
+    #insert single value from list of dictionary
+    data=('Tata Motors Ltd','TATAMOTORS','NSE')
+    #data = {'StockName': 'Tata Motors Ltd','StockCode':'TATAMOTORS','exchange':'NSE'}
+    mysql.InsertUpdateData(query, data)
+    queryDic = ("INSERT INTO stocksdb.StocksList ( StockName,StockCode,exchange) "
+             "VALUES (%(StockName)s, %(StockCode)s, %(exchange)s)")
+    #data=('Tata Motors Ltd','TATAMOTORS','NSE')
+
+
+    #save the dataframe to table
+    data = [('Tata Motors Ltd', 'TATAMOTORS', 'NSE'),
+           ('Microsoft', 'Micro', 'NASDAQ')]
+    mysql.SaveDFToTable(query, data)
+
+
+    #save data from multple value from dictionary to table
+    # dataDictstocks = {
+    #     'StockName': ['Tata Motors Ltd1','Microsoft1'],
+    #     'StockCode': ['TATAMOTORS1','Micro1'],
+    #     'exchange': ['NSE','NASDAQ']
+    # }
+    # mysql.SaveDictionayToTable(queryDic, dataDictstocks)
+
+
+    #mysql.ExecuteDDLStatement("CREATE TABLE stocksdb.tet ( ID INT NOT NULL, Date date NOT NULL)")
